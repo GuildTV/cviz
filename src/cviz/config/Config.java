239d6237ff9be1d6ead9dddf7bf4e141c4516bda
Julian Waller <git@julusian.co.uk>
2016-11-15 00:30:41 +0000

    Fix up some config bugs

--- src/cviz/config/Config.java
+++ src/cviz/config/Config.java
@@ -12,7 +12,7 @@ public class Config implements Serializable {
 
     private TimelineConfig[] channels;
 
-    public Config (int port, int oscPort, String casparHost, int casparPort, TimelineConfig[] channels){
+    public Config(int port, int oscPort, String casparHost, int casparPort, TimelineConfig[] channels) {
         this.port = port;
         this.oscPort = oscPort;
         this.casparHost = casparHost;
@@ -20,44 +20,44 @@ public class Config implements Serializable {
         this.channels = channels;
     }
 
-    public int getPort(){
+    public int getPort() {
         if (port <= 0)
             return 3456;
 
         return port;
     }
 
-    public int getOscPort(){
+    public int getOscPort() {
         if (oscPort <= 0)
             return 5253;
 
         return oscPort;
     }
 
-    public String getCasparHost(){
-        if (casparHost == null || casparHost == "")
+    public String getCasparHost() {
+        if (casparHost == null || casparHost.length() == 0)
             return "127.0.0.1";
 
         return casparHost;
     }
 
-    public int getCasparPort(){
-        if (oscPort <= 0)
+    public int getCasparPort() {
+        if (casparPort <= 0)
             return 5250;
 
-        return oscPort;
+        return casparPort;
     }
 
-    public TimelineConfig[] getChannels(){
+    public TimelineConfig[] getChannels() {
         if (channels == null)
-            return new TimelineConfig[0];
+            return new TimelineConfig[]{new TimelineConfig("default", 1)};
 
         return channels;
     }
 
-    public TimelineConfig findChannelById(String id){
-        for(TimelineConfig channel: channels){
-            if (channel.getId() == id)
+    public TimelineConfig findChannelById(String id) {
+        for (TimelineConfig channel : channels) {
+            if (channel.getId().equals(id))
                 return channel;
         }
 
